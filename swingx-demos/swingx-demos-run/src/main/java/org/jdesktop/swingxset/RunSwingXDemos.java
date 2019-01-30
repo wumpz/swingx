c6c927f56590f720e50b28c560167c60f6150de3
Tobias Warneke <t.warneke@gmx.net>
2019-01-30 08:18:31 +0100

    included swingx-demo-run project to avoid circular dependency between demos content and demos swingxset

---
+++ swingx-demos/swingx-demos-run/src/main/java/org/jdesktop/swingxset/RunSwingXDemos.java
@@ -0,0 +1,14 @@
+/*
+ * Gingko.Systeme (c) 2018
+ */
+package org.jdesktop.swingxset;
+
+/**
+ *
+ * @author tw
+ */
+public class RunSwingXDemos {
+    public static void main(String[] args) {
+        SwingXSet.main(args);
+    }
+}
